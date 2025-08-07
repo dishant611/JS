51464265413a9c31a5223c15989ea0efc9b4362a
Dishant <95769859+dishant611@users.noreply.github.com>
2025-08-07 06:17:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "55ad"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+// "55" => 55
+// "55ad" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "fgj" => true
+
+let someNumber = 56
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
+console.log(stringNumber)
