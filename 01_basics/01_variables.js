ddd4d4b7af06260d5103ec63b9e1d8580441df56
Dishant <95769859+dishant611@users.noreply.github.com>
2025-08-07 06:16:54 +0000

    datatpes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 1234546
+let accountEmail = "dishant@.com"
+var accountPassword = "ffff45"
+accountCity = "surat"
+let accountState
+
+// accountId = 4654 // not allowed
+
+/*
+not use var
+because of issue in block scope and functional scope
+*/
+
+ 
+accountEmail = "ramani@.com"
+accountPassword = "g6f54"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we aer using nodejs nor browsers
+
+let age = 18            // number => 2 to power 53  // bigint
+let name = "dishant"    // string = ""
+let isLoggedIn = true   // boolean = true/false
+let state;              // undefined
+let empty = null        // null => standalone value
+                        // symbol => unique
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // object
